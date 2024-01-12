b28b528099ab1fe1f2f361803bba8d991ae65870
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2024-01-11 22:16:14 -0500

    generating strong password

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import click 
+import secrets
+import string
 
 @click.command()
-def strong_password():
-    pass
+@click('--length', default=20, help='Length of the generated strong password')
+def strong_password(length):
+    """Generates a strong random password."""
+    password = strong_password(length)
+    click.echo(f"Strong password: {password}")
+
+def generate_strong_password(length):
+    """Generates a strong password."""
+    characters =  string.digits + string.punctuation
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
+
+
